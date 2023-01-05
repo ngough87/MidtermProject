e6a99484f2513429d663d50e62269f08a2828718
ngough87 <74070200+ngough87@users.noreply.github.com>
2023-01-05 17:29:50 -0600

    add @service @transactional to endagneredstatusimpl and sunexposureimpl

--- Biome/src/main/java/com/skilldistillery/biome/data/EndangeredStatusDAOImpl.java
+++ Biome/src/main/java/com/skilldistillery/biome/data/EndangeredStatusDAOImpl.java
@@ -2,9 +2,13 @@ package com.skilldistillery.biome.data;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
 
 import com.skilldistillery.biome.entities.EndangeredStatus;
-
+@Service
+@Transactional
 public class EndangeredStatusDAOImpl implements EndangeredStatusDAO {
 	
 	@PersistenceContext
--- Biome/src/main/java/com/skilldistillery/biome/data/SunExposureDAOImpl.java
+++ Biome/src/main/java/com/skilldistillery/biome/data/SunExposureDAOImpl.java
@@ -2,9 +2,13 @@ package com.skilldistillery.biome.data;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
 
 import com.skilldistillery.biome.entities.SunExposure;
-
+@Service
+@Transactional
 public class SunExposureDAOImpl implements SunExposureDAO {
 
 	@PersistenceContext
