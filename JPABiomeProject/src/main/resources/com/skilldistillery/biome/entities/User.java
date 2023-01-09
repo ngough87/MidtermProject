a6986ead77ebdfa0816263800c2708d10b0dab21
Jay-tray <113144309+Jay-tray@users.noreply.github.com>
2023-01-09 13:33:52 -0700

    add sighting method operational

--- JPABiomeProject/src/main/resources/com/skilldistillery/biome/entities/User.java
+++ JPABiomeProject/src/main/resources/com/skilldistillery/biome/entities/User.java
@@ -7,12 +7,12 @@ import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -71,7 +71,7 @@ public class User {
 	private List<Comment> comments;
 	
 	
-	@OneToMany(mappedBy = "user")
+	@OneToMany(mappedBy = "user", fetch=FetchType.EAGER)
 	private List<Sighting> sightings;
 	
 	
